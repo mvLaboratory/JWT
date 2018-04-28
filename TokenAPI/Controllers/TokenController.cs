a8872521979cbde1f56efb877c5bd0d2b953b4a1
Volodymyr Mykytiuk <mvlabaratory@gmail.com>
2018-04-28 22:33:13 +0300

    init commit

---
+++ TokenAPI/Controllers/TokenController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TokenAPI.Core;
+
+namespace TokenAPI.Controllers
+{
+    [Route("api/token/{action}")]
+    public class TokenController : ApiController
+    {
+        [HttpGet] [ActionName("generate")]
+        public IHttpActionResult Index()
+        {
+            return Ok(_tokenFactory.Generate());
+        }
+
+        [HttpPost] [ActionName("validate")]
+        [Route("api/token/validate")]
+        public IHttpActionResult Validate([FromBody]String param)
+        {
+            _tokenFactory.Validate(param);
+            return Ok();
+        }
+
+        TokenFactory _tokenFactory = new TokenFactory();
+    }
+}
---
+++ TokenAPI/Core/TokenFactory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace TokenAPI.Core
+{
+    public class TokenFactory
+    {
+        public String Generate()
+        {
+            var key = "";
+            using (RandomNumberGenerator generator = new RNGCryptoServiceProvider())
+            {
+                byte[] keyData = new byte[32];
+                generator.GetBytes(keyData);
+                key = Convert.ToBase64String(keyData);
+            }
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var header = new JwtHeader(credentials);
+            var payload = new JwtPayload
+            {
+                { "exp", DateTime.Now.AddHours(24)},
+                { "userId", "23665548"},
+                { "uid", Guid.NewGuid()},
+                { "href", @"http://isqft/documents/123/pdf/9987"}
+            };
+
+            var secToken = new JwtSecurityToken(header, payload);
+            var handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(secToken).ToString();
+        }
+
+        public void Validate(String token)
+        {
+
+        }
+    }
+}
